e9ecb9b67f93b28c3211b292555569730fdaa1a7
Robin Moors <robin.Moors@belgacom.net>
2012-10-24 14:15:12 +0200

    Cleanup Project

--- includes/patient_geg.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['a_pat_nr'])) $_SESSION['pat_nr']=$_POST['a_pat_nr'];
-if (isset($_POST['pat_nr'])) $_SESSION['pat_nr']=$_POST['pat_nr'];
-$pat_geg=mysql_fetch_array(mysql_query("SELECT pat_nr,pat_id,pat_naam,pat_voornaam FROM patienten WHERE pat_nr=".$_SESSION['pat_nr']));
-$_SESSION['pat_nr']=$pat_geg['pat_nr'];
-$_SESSION['pat_id']=$pat_geg['pat_id'];
-$_SESSION['pat_naam']=$pat_geg['pat_naam'];
-$_SESSION['pat_voornaam']=$pat_geg['pat_voornaam'];
-?>
