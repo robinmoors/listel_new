3e640971d7eba6064c2253de2147d650cc370ff9
joris <joris@192.168.1.7>
2013-04-27 17:58:00 +0200

    v1.5.5 logins

---
+++ ecareplan/database/util/sample_values3_logins.sql
@@ -0,0 +1,12 @@
+--
+-- Gegevens worden uitgevoerd voor tabel `logins`
+--
+
+INSERT INTO `logins` (`id`, `voornaam`, `naam`, `profiel`, `organisatie`, `adres`, `gem_id`, `tel`, `fax`, `gsm`, `email`, `tp_project`, `sit_id`, `overleg_gemeente`, `login`, `paswoord`, `pwd_recovery`, `ipadres`, `logindatum`, `actief`) VALUES
+(468, 'Joris', 'Jacobs', 'listel', 12913, '', 9999, '', '', '', '', NULL, NULL, NULL, 'joris-listel', 'c17a1a963e2b9ebb228030c0615fdb4bd91bd982', '', '::1', 1348656575, 1),
+(469, 'Joris-ocwm', 'Jacobs', 'OC', 12535, '', 9999, '', '', '', '', NULL, 6, 3500, 'joris-ocmw', 'c17a1a963e2b9ebb228030c0615fdb4bd91bd982', '', '::1', 1354890919, 1),
+(470, 'Joris-rdc', 'Jacobs', 'rdc', 13288, '', 9999, '', '', '', '', NULL, NULL, NULL, 'joris-rdc', 'c17a1a963e2b9ebb228030c0615fdb4bd91bd982', '', '', 0, 1),
+(471, 'Joris', 'Jacobs ', 'psy', 12663, '', 9999, '', '', '', '', NULL, NULL, NULL, 'joris-psy', 'c17a1a963e2b9ebb228030c0615fdb4bd91bd982', '', '::1', 1349808416, 1),
+(472, 'Joris-TP', 'Jacobs', 'hoofdproject', 13453, '', 9999, '', '', '', '', 14, NULL, NULL, 'joris-tp', 'c17a1a963e2b9ebb228030c0615fdb4bd91bd982', '', '127.0.0.1', 1345188390, 1),
+(473, 'Joris-menos', 'Jacobs', 'menos', 12913, '', 9999, '', '', '', '', NULL, NULL, NULL, 'joris-menos', 'c17a1a963e2b9ebb228030c0615fdb4bd91bd982', '', '127.0.0.1', 1345187594, 1);
+
